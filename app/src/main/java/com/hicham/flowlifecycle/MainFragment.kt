9de41308b369495289ff367ab23c02cc42d2405b
hicham <hicham.boushaba@gmail.com>
2021-10-30 13:07:50 +0100

    First import

---
+++ app/src/main/java/com/hicham/flowlifecycle/MainFragment.kt
@@ -0,0 +1,12 @@
+package com.hicham.flowlifecycle
+
+import android.os.Bundle
+import android.view.View
+import androidx.fragment.app.Fragment
+
+class MainFragment: Fragment(R.layout.fragment_main) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
